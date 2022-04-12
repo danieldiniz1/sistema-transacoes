c3c8987c8b3a0328c0585bc389beb08a7fdae5d7
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-12 11:35:09 -0300

    testando rota /home e controller

---
+++ src/main/java/br/com/challenge/sistematransacoes/controller/HomeController.java
@@ -0,0 +1,18 @@
+package br.com.challenge.sistematransacoes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String home(){
+        return "Hello World";
+    }
+}
