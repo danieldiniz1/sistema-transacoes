77efeb0422bb0644c83cd0e08a84e962bbdfe43f
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-12 10:15:48 -0300

    first commit

---
+++ src/main/java/br/com/challenge/sistematransacoes/SistemaTransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.sistematransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/sistematransacoes/SistemaTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.sistematransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
